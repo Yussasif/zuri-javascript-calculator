6d1cb71002f179b5df61bd715e1693629c26edee
Yussasif <99269469+Yussasif@users.noreply.github.com>
2022-07-05 19:12:32 -0700

    fix prompt

--- calculator.js
+++ calculator.js
@@ -1,2 +1,20 @@
-calc=eval(prompt("Enter Expression: "));
-alert(calc);
+let firstNumber = parseInt(prompt("Enter First Number"))
+let operator = prompt("Enter OPerator *+-/: ");
+let secondNumber = parseInt(prompt("Enter Second Number"));
+let result;
+if (typeof firstNumber != "number" && typeof secondNumber != "number") {
+    alert("Enter a valid Number")
+    }
+    else{
+        if (operator == "*" || operator == "-" || operator == "%" ||operator == "+"|| operator == "/"){
+        
+            result = firstNumber + operator + secondNumber;
+            alert(eval(result));
+           
+        }
+        else{
+            alert("Enter a valid Operator")
+        }
+    }
+
+
